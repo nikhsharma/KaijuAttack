f0ad19468275137cfea5b66e7d014db5f9fd55f7
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-07 16:43:53 +0100

    Create LandLizard class. Inherits from Kaiju.

---
+++ src/main/java/LandLizard.java
@@ -0,0 +1,6 @@
+public class LandLizard extends Kaiju {
+
+    public LandLizard(String name, int healthValue, int attackValue) {
+        super(name, healthValue, attackValue);
+    }
+}
---
+++ src/test/java/LandLizardTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LandLizardTest {
+
+    LandLizard landLizard;
+
+    @Before
+    public void before() {
+        landLizard = new LandLizard("Godzilla", 100, 20);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Godzilla", landLizard.getName());
+    }
+
+    @Test
+    public void hasHealthValue() {
+        assertEquals(100, landLizard.getHealthValue());
+    }
+
+    @Test
+    public void hasAttackValue() {
+        assertEquals(20, landLizard.getAttackValue());
+    }
+}
